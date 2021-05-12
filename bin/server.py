c0274f7a179668a1ccc8c7617a62f37e9ac4c2a9
Aaron Gill-Braun <aaron.gill-braun@vidyard.com>
2021-05-12 16:25:27 -0400

    Moved server host & port to .env file

--- bin/server.py
+++ bin/server.py
@@ -1,7 +1,13 @@
 import asyncio
+import dotenv
+import os
 from jc import server
 
 if __name__ == '__main__':
-  server = server.Server('localhost', 1234)
+  dotenv.load_dotenv(".env")
+  host = os.getenv('SERVER_HOST')
+  port = int(os.getenv('SERVER_PORT'))
+
+  server = server.Server(host, port)
   asyncio.get_event_loop().run_until_complete(server.serve())
   asyncio.get_event_loop().run_forever()
--- jc/server/__init__.py
+++ jc/server/__init__.py
@@ -1,5 +1,2 @@
-import dotenv
 from jc.server import server
-dotenv.load_dotenv(".env")
-
 Server = server.Server
